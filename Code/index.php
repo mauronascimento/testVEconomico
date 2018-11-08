25b704ca6c2719519246fd37cab0af253c42f3f8
mauronascimento <mauronmatias@gmail.com>
2018-11-08 03:11:02 -0200

    include vendor

--- Code/index.php
+++ Code/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'estou dentro do code para iniciar app';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response, array $args) {
+echo 'estou da funcao dentro do code para iniciar app';
+
+});
+$app->run();
